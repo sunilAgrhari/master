dd3807437e7ddc97c0248855bbac177abf65e72a
sunilAgrhari <agrahari.sunil@gmail.com>
2018-10-17 17:24:18 +0530

    first commit

---
+++ src/main/java/com/tsys/fullApp/FullAppApplication.java
@@ -0,0 +1,12 @@
+package com.tsys.fullApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullAppApplication.class, args);
+	}
+}
